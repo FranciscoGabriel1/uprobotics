787f861fe4b7a984d68f8023468aabfdd5bc5b87
FranciscoGabriel1 <franciscogabriel.am@gmail.com>
2021-10-11 09:50:30 -0400

    UpInNextJS

--- uprobotics-web/src/pages/Create/index.jsx
+++ uprobotics-web/src/pages/Create/index.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from "react";
+import Card from "react-bootstrap/Card";
 
-
-export function Create() { 
+export function Create() {
   return (
-    <main className="container flex flex--centro flex--coluna" >
-     <h1>Create</h1>
+    <main className="container flex flex--centro flex--coluna">
+      <h1>Create</h1>
+
+      <Card body>This is some text within a card body.</Card>
+      
     </main>
-  )
+  );
 }
 
-export default Create
+export default Create;
