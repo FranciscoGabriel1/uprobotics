255d54872cc26e57bc421afb22d8e02e642c0334
FranciscoGabriel1 <franciscogabriel.am@gmail.com>
2021-10-13 10:57:09 -0400

    update files

--- uprobotics-web-nextjs/src/components/SubscribeButton/index.tsx
+++ uprobotics-web-nextjs/src/components/SubscribeButton/index.tsx
@@ -1,15 +1,15 @@
 // import styles from './styles.module.scss'
 
-import { SubscribeButtonCustom } from "./style";
-export function SubscribeButton(){
-
-    return (
-      <SubscribeButtonCustom
-        type="button"
-        >
-
-
+import { SubscribeButtonCustom } from './style'
+import  Link  from 'next/link'
+export function SubscribeButton() {
+  return (
+    <Link href="/tutorial">
+      <a>
+        <SubscribeButtonCustom type="button">
             Veja como funciona
         </SubscribeButtonCustom>
-    );
+      </a>
+    </Link>
+  )
 }
