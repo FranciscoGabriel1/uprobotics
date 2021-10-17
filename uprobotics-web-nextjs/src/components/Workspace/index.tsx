13f266f4234c726dcc644d094478b31ee410d684
FranciscoGabriel1 <franciscogabriel.am@gmail.com>
2021-10-17 17:23:07 -0400

    iframe teste size

--- uprobotics-web-nextjs/src/components/Workspace/index.tsx
+++ uprobotics-web-nextjs/src/components/Workspace/index.tsx
@@ -1,13 +1,19 @@
 import { Row, Col, Container } from 'react-bootstrap'
 
-import { WorkstationStyle, OutputCode, GridStyled } from './style'
+import { WorkstationStyle, OutputCode, GridStyled, Iframe } from './style'
 
 const Workspace = () => {
   return (
     <>
-      {/* <main>
+    <Iframe
+              dangerouslySetInnerHTML={{
+                __html:
+                  "<iframe src='https://workstation-ecru.vercel.app/' style='margin:0; padding:3; width:100%; height:800px; overflow-y: hidden;' scrolling='no' />"
+              }}
+            />
+      <main>
         <section>
-          <GridStyled>
+          {/* <GridStyled>
             <Row>
               <Col xl={8}>
                 <WorkstationStyle id="blocklyDiv" />
@@ -17,13 +23,14 @@ const Workspace = () => {
                 <OutputCode id="codigogerado" />
               </Col>
             </Row>
+          </GridStyled> */}
+          <GridStyled>
+            
           </GridStyled>
         </section>
-      </main> */}
+      </main>
 
-<Container><div dangerouslySetInnerHTML={{ __html: "<iframe src='https://workstation-ecru.vercel.app/' style='margin:0; padding:0; width:100%; height:800px;' />"}} /></Container>
-
-
+      {/* <Container><div dangerouslySetInnerHTML={{ __html: "<iframe src='https://robotbenchmark.net/benchmark/pick_and_place/simulation.php' style='margin:0; padding:0; width:100%; height:800px;' />"}} /></Container>  */}
     </>
   )
 }
